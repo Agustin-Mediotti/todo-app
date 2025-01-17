186b2b3e0d2ecf2769060c5a73f26fc9fa651c69
Vitto A. Mediotti <agustinmediotti@gmail.com>
2025-01-17 19:57:30 -0300

    fixed: tests for plain text persistency

--- src/app.rs
+++ src/app.rs
@@ -393,8 +393,13 @@ impl App {
 mod tests {
     use super::*;
 
+    fn tests_cleanup() {
+        fs::remove_file("user_data").expect("Failed to remove test file");
+    }
+
     #[test]
     fn add_correct_task() {
+        tests_cleanup();
         let mut app = App::new().expect("error instanciating app");
         app.clean_tasks().expect("error while removing all tasks");
         assert_eq!(
@@ -430,51 +435,54 @@ mod tests {
 
     #[test]
     fn display_tasks() {
+        tests_cleanup();
         let mut app = App::new().expect("error instanciating app");
         app.clean_tasks().expect("error while removing all tasks");
         app.add_task(
             Task::new(
                 app.tasks.len(),
                 String::from("Hello World"),
-                String::from("hello world"),
+                String::from("Hello World"),
             )
             .expect("error creating new task"),
         )
         .expect("error while adding a new task");
-        assert_eq!(app.tasks_into_string(), "Hello World [] \n")
+        assert_eq!(app.tasks_into_string(), "Hello World [] Hello World \n");
     }
 
     #[test]
     fn remove_task() {
+        tests_cleanup();
         let mut app = App::new().expect("error instanciating app");
         app.clean_tasks().expect("error while removing all tasks");
         app.add_task(
             Task::new(
                 app.tasks.len(),
                 String::from("Hello World"),
-                String::from("hello world"),
+                String::from("Hello World"),
             )
             .expect("error creating new task"),
         )
         .expect("error while adding a new task");
         app.remove_task(0).expect("error while removing task");
-        assert_eq!(app.tasks_into_string(), "")
+        assert_eq!(app.tasks_into_string(), "");
     }
 
     #[test]
     fn remove_all_tasks() {
+        tests_cleanup();
         let mut app = App::new().expect("error instanciating app");
         app.clean_tasks().expect("error while removing all tasks");
         app.add_task(
             Task::new(
                 app.tasks.len(),
                 String::from("Hello World"),
-                String::from("hello world"),
+                String::from("Hello World"),
             )
             .expect("error creating new task"),
         )
         .expect("error while adding a new task");
         app.clean_tasks().expect("error while removing all tasks");
-        assert_eq!(app.tasks_into_string(), "")
+        assert_eq!(app.tasks_into_string(), "");
     }
 }
